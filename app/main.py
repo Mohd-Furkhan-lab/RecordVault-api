3ca5d69637868eb8e8b48d9d0b7b8dae409bcff9
Mohd Furkhan <mohdfurkhan2255@gmail.com>
2026-05-12 10:52:49 +0530

    Added Cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from apis.v1.records import records
 from apis.v1.users import users
-from repo.db import engine,Base
+from repo.db import engine, Base
 from create_admin import createadmin
 
-app=FastAPI()
+app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "https://your-frontend-domain.com",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(records)
 app.include_router(users)
 
@@ -13,8 +28,5 @@ def startup():
     Base.metadata.create_all(bind=engine)
     createadmin()
 
-
 def get_app():
     return app
-
-    
